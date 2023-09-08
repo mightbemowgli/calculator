123101e3e00870f08da59fdbda892191735d84cc
Michael Schillinger <mithschill@gmail.com>
2023-09-08 13:44:01 -0500

    Add basic math operators and operate function

---
+++ script.js
@@ -0,0 +1,33 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+const operate = (operator, a, b) => {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '÷':
+            if (b === 0) return null;
+            else return divide(a, b);
+        default:
+            return null;
+    }
+}
