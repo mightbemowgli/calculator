e012b6f6d1f9d62c30c3730db08c82f2457533f4
Michael Schillinger <mithschill@gmail.com>
2023-09-08 19:29:28 -0500

    Add button selectors and event handler functions

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = '';
+let shouldReset = false;
+
+const lastOpScreen = document.getElementById('lastOpScreen');
+const currentOpScreen = document.getElementById('currentOpScreen');
+const numberButtons = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.op');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('delBtn');
+const decimalButton = document.getElementById('decimal');
+const equalsButton = document.getElementById('equalsBtn');
+
+numberButtons.forEach(button => button.addEventListener('click', () => appendNumber(button.textContent)));
+operatorButtons.forEach(button => button.addEventListener('click', () => appendNumber(button.textContent)));
+clearButton.addEventListener('click', () => clearScreen());
+
+const resetScreen = () => {
+    currentOpScreen.textContent = '';
+    shouldReset = false;
+}
+
+const clearScreen = () => {
+    lastOpScreen.textContent = '';
+    currentOpScreen.textContent = '0';
+}
+
+const appendNumber = num => {
+    if (currentOpScreen.textContent === '0' || shouldReset)
+        resetScreen();
+        currentOpScreen.textContent += num;
+}
+
 const add = (a, b) => {
     return a + b;
 }
